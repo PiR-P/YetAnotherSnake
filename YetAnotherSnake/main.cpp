94f7bae9d4032ebdc905a0d31b50a36aa4ba4570
Pierre Pasquet <p.pasquet@warwick.ac.uk>
2018-01-02 18:36:03 +0100

    Initial Commit

---
+++ YetAnotherSnake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  YetAnotherSnake
+//
+//  Created by Pierre Pasquet on 02/01/2018.
+//  Copyright © 2018 Pierre Pasquet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
